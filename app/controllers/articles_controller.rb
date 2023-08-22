56abb1eb10f9a3a4598eeb4efcae99a77f749e43
Shokr trails <mahmoudshoker11@yahoo.com>
2023-08-22 11:58:56 +0300

    adding list all  articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,14 +4,31 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def index
+        @article = Article.all
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article has been created succefully"
             redirect_to article_path(@article)
         else
-            render 'new'
+            render :new, status: 422
         end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Artcle has been updated"
+            redirect_to article_path(@article)
+        else
+            render :edit, status: 422
+        end    
     end
 
     def show
